e3a4dd4546e2dc435f297469bc0d85ed0352f251
Bernard Teles <bernardtelgon@gmail.com>
2024-04-08 16:54:17 -0300

    collection router

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  validates :name, :address, :rating, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    get 'top', on: :collection
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
 end
