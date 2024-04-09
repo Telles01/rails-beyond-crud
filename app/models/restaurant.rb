bd981c3a9414bd686f18e0dc2c39c63d8c89b996
Bernard Teles <bernardtelgon@gmail.com>
2024-04-09 05:30:07 -0300

    reviews sistem

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
   validates :name, :address, :rating, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :restaurants do
     get 'top', on: :collection
+    get 'chef', on: :member
+
+
+    resources :reviews, only: [:new, :create]
   end
 
   root "restaurants#index"
